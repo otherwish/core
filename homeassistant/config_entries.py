df5377a7cf6fbc4a7674270cbf5d74f5956805c2
G Johansson <goran.johansson@shiftit.se>
2024-02-04 23:10:17 +0000

    Fix reconfigure

--- homeassistant/config_entries.py
+++ homeassistant/config_entries.py
@@ -920,7 +920,7 @@ class ConfigEntry:
             # Reconfigure flow already in progress for this entry
             return
         hass.async_create_task(
-            self._async_init_reauth(hass, context, data),
+            self._async_init_reconfigure(hass, context, data),
             f"config entry reconfigure {self.title} {self.domain} {self.entry_id}",
         )
 
@@ -2418,9 +2418,11 @@ async def support_remove_from_device(hass: HomeAssistant, domain: str) -> bool:
 
 
 async def support_entry_reconfigure(hass: HomeAssistant, domain: str) -> bool:
-    """Test if a domain supports entry unloading."""
-    if loader.is_component_module_loaded(hass, f"{domain}.config_flow") and (
-        HANDLERS.get(domain)
+    """Test if a domain supports reconfigure flow."""
+    if (
+        loader.is_component_module_loaded(hass, f"{domain}.config_flow")
+        and (handler := HANDLERS.get(domain))
+        and hasattr(handler, "async_step_reconfigure")
     ):
         return True
     return False
